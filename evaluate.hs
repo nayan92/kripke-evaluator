390e5850beff90fa17284ad2c832d2055c8aa918
Nayan Chudasama <nayan.chudasama11@gmail.com>
2013-01-15 16:25:54 +0000

    Completed 2nd case of that list of cases

--- evaluate.hs
+++ evaluate.hs
@@ -1,14 +1,22 @@
-data Atom = Var String
+import Data.Maybe
 
-data Formula = Atom | True | Not Formula | And Formula Formula | Box Formula | Diamond Formula
+type Atom = String
+data Formula = Var Atom | TrueT | FalseT | Not Formula | And Formula Formula | Box Formula | Diamond Formula
+  deriving (Eq, Ord, Show)
 
 type Node = String
 type Nodes = [ Node ]
-type Variables = [ Atom ]
+--type Variables = [ Atom ]
 type Relation = [ ( Node, Node ) ]
 type Valuation = [ ( Atom, Node ) ]
 
 type Model = ( Nodes, Relation, Valuation )
 
---evaluate :: Model -> Formula
---evaluate(_,_) = True
+evaluate :: Model -> Node -> Formula -> Bool
+evaluate _ _ TrueT
+  = True
+evaluate ( _, _, val ) w ( Var v )
+  = elem w ( snd ( unzip ( filter matches val ) ) )
+  where
+    matches ( v', _ )
+      = v' == v
