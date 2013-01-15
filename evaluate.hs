7015edd78abb06f1b52b69665727bc2474e74432
Nayan Chudasama <nayan.chudasama11@gmail.com>
2013-01-15 17:23:43 +0000

    Added some comments

--- evaluate.hs
+++ evaluate.hs
@@ -1,35 +1,46 @@
+----------------------
+-- Kripke Evaluator --
+----------------------
+
 type Atom = String
 
 data Formula = Var Atom | TrueT | FalseT | Not Formula | And Formula Formula | Box Formula | Diamond Formula | Implies Formula Formula
 
 type Node = String
-type Nodes = [ Node ]
---type Variables = [ Atom ]
+type World = [ Node ]
 type Relation = [ ( Node, Node ) ]
 type Valuation = [ ( Atom, Node ) ]
 
-type Model = ( Nodes, Relation, Valuation )
+type Model = ( World, Relation, Valuation )
 
 evaluate :: Model -> Node -> Formula -> Bool
+-- Case when (M, w) |= true
 evaluate _ _ TrueT
   = True
+-- Case when (M, w) |= p
 evaluate ( _, _, val ) w ( Var v )
   = elem w ( snd ( unzip ( filter matches val ) ) )
   where
     matches ( v', _ )
       = v' == v
+-- Case when (M, w) |= not f
 evaluate m w ( Not f )
   = not ( evaluate m w f )
+-- Case when (M, w) |= f and f'
 evaluate m w ( And f f' )
   = ( evaluate m w f ) && ( evaluate m w f' )
+-- Case when (M, w) |= box f
 evaluate m@( n, r, _ ) w ( Box f )
   = and ( map evaluate' n )
   where
     evaluate' w'
       = ( not ( elem ( w, w' ) r ) ) || ( evaluate m w' f )
+-- Case when (M, w) |= diamond f
 evaluate m w ( Diamond f )
   = evaluate m w ( Not ( Box ( Not f ) ) )
+-- Case when (M, w) |= f -> f'
 evaluate m w ( Implies f f' )
   = ( not ( evaluate m w f ) ) || ( evaluate m w f' )
+-- Case else
 evaluate _ _ _
   = False
